e46c5da16dc22b01325a953f0c93bd10374aca2b
Mufei Zhao <915177778@qq.com>
2023-04-11 16:51:47 +0800

    Update shortened_grace_period_test.go

--- test/e2e_node/shortened_grace_period_test.go
+++ test/e2e_node/shortened_grace_period_test.go
@@ -17,13 +17,19 @@ limitations under the License.
 package e2enode
 
 import (
+	"bytes"
 	"context"
 	"github.com/onsi/ginkgo/v2"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/dynamic"
+	watchtools "k8s.io/client-go/tools/watch"
 	"k8s.io/kubernetes/test/e2e/framework"
 	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
 	admissionapi "k8s.io/pod-security-admission/api"
+	"strings"
 	"time"
 )
 
@@ -32,22 +38,52 @@ var _ = SIGDescribe("Shortened Grace Period", func() {
 	f.NamespacePodSecurityEnforceLevel = admissionapi.LevelBaseline
 	ginkgo.Context("When repeatedly deleting pods", func() {
 		var podClient *e2epod.PodClient
+		var dc dynamic.Interface
+		var ns string
+		var podName = "test"
+		var ctx = context.Background()
+		var rcResource = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "Pod"}
 		ginkgo.BeforeEach(func() {
+			ns = f.Namespace.Name
+			dc = f.DynamicClient
 			podClient = e2epod.NewPodClient(f)
 		})
 		ginkgo.It("shorter grace period of a second command overrides the longer grace period of a first command", func() {
-			const (
-				gracePeriod      = 10000
-				gracePeriodShort = 10
-			)
-			ctx := context.Background()
-			podName := "test"
-			podClient.CreateSync(ctx, getGracePeriodTestPod(podName, gracePeriod))
-			err := podClient.Delete(ctx, podName, *metav1.NewDeleteOptions(gracePeriod))
-			framework.ExpectNoError(err)
-			start := time.Now()
-			podClient.DeleteSync(ctx, podName, *metav1.NewDeleteOptions(gracePeriodShort), gracePeriod*time.Second)
-			framework.ExpectEqual(time.Since(start) < gracePeriod*time.Second, true, "Failure to shorten grace period")
+			expectedWatchEvents := []watch.Event{
+				{Type: watch.Deleted},
+			}
+			var exitCode int32
+			callback := func(retryWatcher *watchtools.RetryWatcher) (actualWatchEvents []watch.Event) {
+				pod, err := podClient.Get(ctx, podName, metav1.GetOptions{})
+				framework.ExpectNoError(err, "failed to get pod %q", podName)
+				//Verify exit code
+				exitCode = pod.Status.ContainerStatuses[0].State.Terminated.ExitCode
+				framework.ExpectNoError(err, "failed to get most recent container exit code for pod %q", podName)
+				framework.ExpectEqual(exitCode, int32(0), "unexpected container exit code for pod %q.code is %d", podName, exitCode)
+				// Get pod logs.
+				logs, err := podClient.GetLogs(podName, &v1.PodLogOptions{}).Stream(ctx)
+				framework.ExpectNoError(err, "failed to get pod logs")
+				defer logs.Close()
+				buf := new(bytes.Buffer)
+				buf.ReadFrom(logs)
+				podLogs := buf.String()
+				// Verify the number of SIGINT
+				SIGINT1 := strings.Count("SIGINT 1", podLogs)
+				SIGINT2 := strings.Count("SIGINT 2", podLogs)
+				framework.ExpectEqual(SIGINT1, int32(1), "Unexpected SIGINT 1 exit volume")
+				framework.ExpectEqual(SIGINT2, int32(1), "Unexpected SIGINT 1 exit volume")
+				return nil
+			}
+			framework.WatchEventSequenceVerifier(ctx, dc, rcResource, ns, podName, metav1.ListOptions{LabelSelector: "test=true"}, expectedWatchEvents, callback, func() (err error) {
+				const (
+					gracePeriod      = 10000
+					gracePeriodShort = 20
+				)
+				podClient.CreateSync(ctx, getGracePeriodTestPod(podName, gracePeriod))
+				err = podClient.Delete(ctx, podName, *metav1.NewDeleteOptions(gracePeriod))
+				podClient.DeleteSync(ctx, podName, *metav1.NewDeleteOptions(gracePeriodShort), gracePeriod*time.Second)
+				return err
+			})
 		})
 	})
 })
@@ -60,6 +96,9 @@ func getGracePeriodTestPod(name string, gracePeriod int64) *v1.Pod {
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
+			Labels: map[string]string{
+				"test": "true",
+			},
 		},
 		Spec: v1.PodSpec{
 			Containers: []v1.Container{
@@ -70,13 +109,13 @@ func getGracePeriodTestPod(name string, gracePeriod int64) *v1.Pod {
 					Args: []string{`
 term() {
   if [ "$COUNT" -eq 0 ]; then
-    echo "SIGINT 1"
+    echo "SIGINT 1" >> /dev/termination-log
   elif [ "$COUNT" -eq 1 ]; then
-    echo "SIGINT 2"
+    echo "SIGINT 2" >> /dev/termination-log
     sleep 5
     exit 0
   else
-    echo "SIGINT $COUNT"
+    echo "SIGINT $COUNT" >> /dev/termination-log
     exit 1
   fi
   COUNT=$((COUNT + 1))
